e5589147e2e730ec37c5027e94f47aa35e1a72dc
Ultra-Lutetia <appultralutetia@gmail.com>
2026-06-17 23:36:24 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 
 const UL_CONFIG = {
   SUPABASE_URL: 'https://afgriuvrtdkklluvtswg.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_nE0Xqxqx8DTRalIKGsqAMA_Gmnvclh7',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmZ3JpdXZydGRra2xsdXZ0c3dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MTkwODgsImV4cCI6MjA5NzI5NTA4OH0.3jXUyJD87MjhFJctzceMVoeHWGCSqGmVy3TPuXGQebc',
   APP_VERSION: '1.0.0',
   SAISON_COURANTE: '2026-2027',
 };
